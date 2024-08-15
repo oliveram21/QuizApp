a18985564ae8ddcf1d3be357189e34a7699f5672
Olivera Miatovici <oliveram21@yahoo.com>
2024-08-15 12:15:50 +0300

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Olivera Miatovici on 15.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
