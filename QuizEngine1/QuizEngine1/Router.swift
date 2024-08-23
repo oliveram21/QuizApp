6bc957f3ff6357c623df659eb48bdadc0beeadb3
Olivera Miatovici <oliveram21@yahoo.com>
2024-08-23 12:12:20 +0300

    Introduce new protocol QuizDelegate protocol (it will replace Router protocol)

--- QuizEngine1/QuizEngine1/Router.swift
+++ QuizEngine1/QuizEngine1/Router.swift
@@ -7,11 +7,20 @@
 
 import Foundation
 
+public protocol QuizDelegate<Question, Answer> where Question: Hashable {
+    associatedtype Question
+    associatedtype Answer
+    typealias AnswerCallback = (Answer) -> Void
+    
+    func handle(question: Question, answerCallback: @escaping AnswerCallback)
+    func handle(result: Result<Question, Answer>)
+}
+
 @available(*, deprecated)
 public protocol Router<Question, Answer> where Question: Hashable {
     associatedtype Question
     associatedtype Answer
     typealias AnswerCallback = (Answer) -> Void
-    func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
+    func routeTo(question: Question, answerCallback: @escaping AnswerCallback)
     func routeTo(result: Result<Question, Answer>)
 }
