16cfe7830606db982d6962043ffeeaae12eb0320
Olivera Miatovici <oliveram21@yahoo.com>
2024-08-26 07:54:03 +0300

    Remove unnecessary code

--- QuizEngine1/QuizEngine1/Flow.swift
+++ QuizEngine1/QuizEngine1/Flow.swift
@@ -11,14 +11,10 @@ class Flow<Question, Answer, Delegate: QuizDelegate> where Delegate.Answer == An
     private let delegate: Delegate
     private let questions: [Question]
     private var newAnswers: [(Question, Answer)] = []
-    private var answers: [Question: Answer] = [:]
-    private let scoring: ([Question: Answer]) -> Int
-    
    
-    init(questions: [Question], delegate: Delegate, scoring: @escaping ([Question: Answer]) -> Int = { _ in 0}) {
+    init(questions: [Question], delegate: Delegate) {
         self.questions = questions
         self.delegate = delegate
-        self.scoring = scoring
     }
     
     func start() {
@@ -31,22 +27,16 @@ class Flow<Question, Answer, Delegate: QuizDelegate> where Delegate.Answer == An
             delegate.answer(for: question, completion: answer(for: question, at: index))
         } else {
             delegate.didCompleteQuiz(with: newAnswers)
-           // delegate.handle(result: createResult())
         }
     }
     private func answer(for question: Question, at index: Int) -> (Answer) -> Void {
         return { [weak self] answer in
             self?.newAnswers.replaceOrInsert((question, answer), at: index)
-            self?.answers[question] = answer
             self?.delegateQuestionHandling(after: index)
         }
     }
     private func delegateQuestionHandling(after index: Int) {
        delegateQuestionHandling(at: questions.index(after: index))
-    }
-   
-    private func createResult() -> Result<Question, Answer> {
-        return Result(answers: answers, score: scoring(answers))
     }
 }
 
