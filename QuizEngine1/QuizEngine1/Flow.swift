e3a5f2c311c0cd74f8f86767b67befd536471851
Olivera Miatovici <oliveram21@yahoo.com>
2024-08-26 07:56:32 +0300

    Rename new api property

--- QuizEngine1/QuizEngine1/Flow.swift
+++ QuizEngine1/QuizEngine1/Flow.swift
@@ -10,7 +10,7 @@ import Foundation
 class Flow<Question, Answer, Delegate: QuizDelegate> where Delegate.Answer == Answer, Delegate.Question == Question {
     private let delegate: Delegate
     private let questions: [Question]
-    private var newAnswers: [(Question, Answer)] = []
+    private var answers: [(Question, Answer)] = []
    
     init(questions: [Question], delegate: Delegate) {
         self.questions = questions
@@ -26,12 +26,12 @@ class Flow<Question, Answer, Delegate: QuizDelegate> where Delegate.Answer == An
             let question = questions[index]
             delegate.answer(for: question, completion: answer(for: question, at: index))
         } else {
-            delegate.didCompleteQuiz(with: newAnswers)
+            delegate.didCompleteQuiz(with: answers)
         }
     }
     private func answer(for question: Question, at index: Int) -> (Answer) -> Void {
         return { [weak self] answer in
-            self?.newAnswers.replaceOrInsert((question, answer), at: index)
+            self?.answers.replaceOrInsert((question, answer), at: index)
             self?.delegateQuestionHandling(after: index)
         }
     }
